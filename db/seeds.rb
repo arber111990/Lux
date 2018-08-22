804b3b52e4215e6677bd8409cc21d0669b5d6bba
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-08-22 15:37:30 +0100

    made the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,3 @@ User.new(email: "Nick@bogdanoff.com", password: "dompeet").save
 Item.new(id: 1, title: "Nicks Naughty Yacht", location: "Nicks oudoor swimming pool", description: "Spend time on a real yacht with Nick Johnson and...other people", price: 1000, rating: 1, user_id: 2).save
 Item.new(id: 2, title: "Ed Ward's Lambo",location: "lewagon london", description: "Rent out the great man's car!", price: 100, rating: 5, user_id: 2).save
 Review.new(id: 1, user_id: 2, item_id: 1, title: "The greatest dissapointment since Alex Haumer left",description: "Nick promised a yacht, it was actually a dinghy! What a sneaky guy!", rating: 1).save
-

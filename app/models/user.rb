77c582a023422f68612da48fb0a21e88b539940f
Christian Bell <christiantcbell@gmail.com>
2018-08-22 16:20:32 +0100

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :items
-  has_many :reviews
-  has_many :bookings
+  has_many :items, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :bookings, dependent: :destroy
 end

d672a30cc672e9bb1e07bb35ad93d3fe053e0998
Nick Johnson <neecholas.johnson1997@gmail.com>
2018-08-23 15:53:35 +0100

    Made the photo attribute of item

--- db/migrate/20180821131501_create_items.rb
+++ db/migrate/20180821131501_create_items.rb
@@ -3,7 +3,6 @@ class CreateItems < ActiveRecord::Migration[5.2]
     create_table :items do |t|
       t.references :user, foreign_key: true
       t.integer :price
-      t.string :image_url
       t.string :title
       t.string :description
       t.integer :rating

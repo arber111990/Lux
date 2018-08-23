792990815ecce4694ffd05be50958436dfe05a3a
Christian Bell <christiantcbell@gmail.com>
2018-08-23 14:37:05 +0100

    christian js stuff and another commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,11 @@ import "bootstrap";
 
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 initUpdateNavbarOnScroll();
+
+import { openModalLoginSearch } from '../components/modal';
+openModalLoginSearch();
+
+import { openModalLoginList } from '../components/modal';
+openModalLoginList();
+
+

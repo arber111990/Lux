e97468fee28431698a449212be7aae1bd01bde48
Filip Ilinca <filip.ilinca@protonmail.com>
2018-08-24 10:29:50 +0100

    some stuff

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2018_08_23_102035) do
   create_table "items", force: :cascade do |t|
     t.bigint "user_id"
     t.integer "price"
-    t.string "image_url"
     t.string "title"
     t.string "description"
     t.integer "rating"
